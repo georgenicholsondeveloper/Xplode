285498b41060bea4d43005403a0a6e48d81622ce
Unknown <gnicholsoncloud@gmail.com>
2018-10-15 17:22:54 +0100

    Up[date 3
    rotate to point at touch

---
+++ Assets/Scripts/CamScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+    GameObject player;
+    private Vector3 camPos;
+    Camera cam;
+
+    void Start () {
+        cam = gameObject.GetComponent<Camera>();
+        camPos = cam.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	
+	void Update () {
+        Cam();
+    }
+
+    void Cam()
+    {
+        camPos.y = player.transform.position.y;
+        camPos.x = player.transform.position.x;
+        cam.transform.position = camPos;
+    }
+}
---
+++ Assets/Scripts/PlayerCharacterScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterScript : MonoBehaviour {
+    GameObject player;
+    Rigidbody2D playerRigid;
+    Touch touchedPosition;
+    
+    int touches;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRigid = player.GetComponent<Rigidbody2D>();
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Movement();
+	}
+
+    void Movement()
+    {
+
+        touches = Input.touchCount;
+        if (touches > 0)
+        {
+            touchedPosition = Input.GetTouch(0);
+            PointToTouch();
+            GameObject.FindGameObjectWithTag("Touch").GetComponent<TouchTrialScript>().newPos = Camera.main.ScreenToWorldPoint(touchedPosition.position);
+
+            playerRigid.velocity = Vector2.zero;
+            playerRigid.AddForce(-transform.up * 800);
+
+
+
+        }
+    }
+
+    void PointToTouch()
+    {
+        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        Vector2 lookDirection = (touchPosition - (Vector2)transform.position).normalized;
+
+        transform.up = lookDirection;
+    }
+}
