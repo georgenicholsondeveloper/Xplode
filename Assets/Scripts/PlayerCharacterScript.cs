0ba08c3741c51d3dfb4ca49a5ab7cee996b4e545
Unknown <gnicholsoncloud@gmail.com>
2018-10-15 17:25:25 +0100

    Update 4

--- Assets/Scripts/PlayerCharacterScript.cs
+++ Assets/Scripts/PlayerCharacterScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacterScript : MonoBehaviour {
+
     GameObject player;
     Rigidbody2D playerRigid;
     Touch touchedPosition;
